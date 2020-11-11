daafb5db0a1839b57ec8727d7ab01cd3ea08594d
Mateusz Okulus <mmokulus@gmail.com>
2020-11-11 16:27:19 +0100

    Change border to black

--- config.h
+++ config.h
@@ -23,7 +23,7 @@ static const char col_cyan[]        = "#0dbc79";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
